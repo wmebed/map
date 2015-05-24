f450967b242f7fe0a8625cb24bba81accc455f85
mebedw <wade.mebed@cbsinteractive.com>
2015-05-24 11:47:36 -0700

    Initial Commit

---
+++ Map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Map
+//
+//  Created by Wade Mebed on 5/24/15.
+//  Copyright (c) 2015 Wade Mebed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
